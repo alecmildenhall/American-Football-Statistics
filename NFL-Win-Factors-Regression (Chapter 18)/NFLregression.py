29ff9b0bd59aa080a16a3b8def90a2ea0a6bf220
alecmildenhall <alecmildenhall@gmail.com>
2026-01-19 00:10:03 -0800

    Improve and fix chapter 18 files

--- NFL-Win-Factors-Regression (Chapter 18)/NFLregression.py
+++ NFL-Win-Factors-Regression (Chapter 18)/NFLregression.py
@@ -11,19 +11,19 @@ from statsmodels.formula.api import ols
 nfldata = pd.read_csv("NFLdata.csv")
 
 # full regression model
-nflmodel = ols('Margin~RETTD+PENDIF+PY_A+DPY_A+RY_A+DRY_A+TO+DTO-1',data = nfldata).fit()
+nflmodel = ols('Margin~Q("RET TD")+PENDIF+Q("PY/A")+Q("DPY/A")+Q("RY/A")+Q("DRY/A")+TO+DTO-1',data = nfldata).fit()
 print(nflmodel.summary())
 
 # passing-only regression
-nflmodelpassing = ols('Margin~PY_A+DPY_A',data = nfldata).fit()
+nflmodelpassing = ols('Margin~Q("PY/A")+Q("DPY/A")',data = nfldata).fit()
 print(nflmodelpassing.summary())
 
 # rushing-only regression
-nflmodelrushing = ols('Margin~RY_A+DRY_A',data=nfldata).fit()
+nflmodelrushing = ols('Margin~Q("RY/A")+Q("DRY/A")',data=nfldata).fit()
 print(nflmodelrushing.summary())
 
 # correlation matrix
 print("++++++++++++++++++++++ Correlation Matrix ++++++++++++++++++++++")
-corr = nfldata.drop(columns = ['Year','Margin']).corr()
+corr = nfldata.drop(columns = ['Year','Team','Margin']).corr()
 
 print(corr)
